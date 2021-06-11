1b01ddcea011e4811bb6165521f791d22bb2c824
Dmitry Bublik <dmitry.buba@gmail.com>
2021-06-11 21:59:19 +0400

    chore: add domain projects

---
+++ Ngcs.Server/Ngcs.ElasticSearch.Domain.Services/Module.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using System.Diagnostics;
+using Ngcs.Practices.Composition;
+using Ngcs.Practices.IoC;
+
+namespace Ngcs.ElasticSearch.Domain.Services
+{
+    [Export(typeof(ICompositionModule))]
+    class Module : ICompositionModule
+    {
+        public void RegisterModule(IIocContainer container)
+        {
+            Debug.WriteLine(("Module!"));
+        }
+    }
+}
