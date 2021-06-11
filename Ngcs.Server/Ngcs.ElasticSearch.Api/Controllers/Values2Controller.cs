269afd54cb463e307881ca2462771f4f45d3455a
Dmitry Bublik <dmitry.buba@gmail.com>
2021-06-11 22:29:17 +0400

    feat: add api assembly

---
+++ Ngcs.Server/Ngcs.ElasticSearch.Api/Controllers/Values2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Ngcs.ElasticSearch.Domain.Contracts;
+
+namespace Ngcs.ElasticSearch.Api.Controllers
+{
+    public class Values2Controller : ApiController
+    {
+        private readonly IValueService _valueService;
+
+        public Values2Controller(IValueService valueService)
+        {
+            _valueService = valueService;
+        }
+
+        [HttpGet]
+        [Route("~/api/Values2")]
+        public int GetValue()
+        {
+            return _valueService.GetValue() * 2;
+        }
+    }
+}
---
+++ Ngcs.Server/Ngcs.ElasticSearch.Api/Module.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Ngcs.Practices.Composition;
+using Ngcs.Practices.IoC;
+
+namespace Ngcs.ElasticSearch.Api
+{
+    [Export(typeof(ICompositionModule))]
+    class Module : ICompositionModule
+    {
+        public void RegisterModule(IIocContainer container)
+        {
+        }
+    }
+}
