7799d2ccfcdd1116f762469be94521106a56d597
David Kossoglyad <david@logoui.co.il>
2021-06-12 18:31:50 +0300

    chore: cleanup the code

--- Ngcs.Server/NGCS.Practices.Composition.Web/AssembliesResolver.cs
+++ Ngcs.Server/NGCS.Practices.Composition.Web/AssembliesResolver.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Web;
@@ -16,23 +15,21 @@ namespace NGCS.Practices.Composition.Web
 	    {
 	    }
 
-	    protected override IEnumerable<Assembly> GetRootAssemblies() => (IEnumerable<Assembly>)new Assembly[1]
+	    protected override IEnumerable<Assembly> GetRootAssemblies() => new[]
 	    {
-		    AssembliesResolver.GetEntryAssembly()
+		    GetEntryAssembly()
 	    };
 
-	    ICollection<Assembly> IAssembliesResolver.GetAssemblies() => (ICollection<Assembly>)this.GetAssemblies().ToList<Assembly>();
+	    ICollection<Assembly> IAssembliesResolver.GetAssemblies() => GetAssemblies().ToList();
 
 	    private static Assembly GetEntryAssembly()
 	    {
 		    if (HttpContext.Current == null || HttpContext.Current.ApplicationInstance == null)
-			    return (Assembly)null;
-		    Type type = HttpContext.Current.ApplicationInstance.GetType();
-		    while (type != (Type)null && type.BaseType != (Type)null && type.BaseType.Name != "HttpApplication")
+			    return null;
+		    var type = HttpContext.Current.ApplicationInstance.GetType();
+		    while (type?.BaseType != null && type.BaseType.Name != "HttpApplication")
 			    type = type.BaseType;
-		    return type == (Type)null ? (Assembly)null : type.Assembly;
+		    return type.Assembly;
 	    }
     }
 }
-
-
--- Ngcs.Server/Ngcs.Server.Facade/Global.asax.cs
+++ Ngcs.Server/Ngcs.Server.Facade/Global.asax.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace Ngcs.Server.Facade
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+	        GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
