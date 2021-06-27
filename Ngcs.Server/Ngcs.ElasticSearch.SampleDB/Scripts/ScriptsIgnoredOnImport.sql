1dfa09d3d31ef1191198aa1f1b5793143dcdaa79
Dmitry Bublik <dmitry.buba@gmail.com>
2021-06-27 22:23:03 +0400

    chore: change db schema and add insert script

---
+++ Ngcs.Server/Ngcs.ElasticSearch.SampleDB/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,90 @@
+﻿
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (17, N'שלום נצרת', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (18, N'שלום טבריה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (19, N'שלום בית שאן', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (20, N'שלום צפת', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (21, N'שלום עפולה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (22, N'שלום קריית שמונה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (23, N'שלום קצרין', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (24, N'שלום מסעדה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (25, N'שלום קריות', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (26, N'שלום חיפה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (27, N'שלום עכו', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (28, N'שלום חדרה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (30, N'שלום ירושלים', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (31, N'שלום בית שמש', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (32, N'שלום תל אביב - יפו', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (34, N'שלום הרצליה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (35, N'שלום פתח תקווה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (36, N'שלום רמלה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (37, N'שלום רחובות', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (38, N'שלום נתניה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (39, N'שלום כפר סבא', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (40, N'שלום ראשון לציון', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (41, N'שלום באר שבע', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (42, N'שלום אשקלון', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (43, N'שלום אשדוד', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (44, N'שלום קריית גת', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (45, N'שלום דימונה', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (46, N'שלום אילת', N'שלום');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (58, N'עניינים מקומיים אריאל', N'עניינים מקומיים');
+GO
+
+INSERT INTO [dbo].[Courts] ([id], [name], [level]) VALUES (237, N'שלום בת ים', N'שלום');
+GO
--- Ngcs.Server/Ngcs.ElasticSearch.SampleDB/dbo/Tables/Courts.sql
+++ Ngcs.Server/Ngcs.ElasticSearch.SampleDB/dbo/Tables/Courts.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Courts] (
-    [id]    INT           IDENTITY (1, 1) NOT NULL,
+    [id]    INT           NOT NULL,
     [name]  NVARCHAR (50) NOT NULL,
     [level] NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_Courts] PRIMARY KEY CLUSTERED ([id] ASC)
 );
-
