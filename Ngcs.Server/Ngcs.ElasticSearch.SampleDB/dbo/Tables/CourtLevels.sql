8f557a5ca1db7d880b274f2124184c70526c2353
Dmitry Bublik <dmitry.buba@gmail.com>
2021-07-01 15:50:08 +0400

    chore: update data schema in db project

---
+++ Ngcs.Server/Ngcs.ElasticSearch.SampleDB/dbo/Tables/CourtLevels.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[CourtLevels] (
+    [id]    INT           NOT NULL,
+    [name]  NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_CourtLevels] PRIMARY KEY CLUSTERED ([id] ASC)
+);
--- Ngcs.Server/Ngcs.ElasticSearch.SampleDB/dbo/Tables/Courts.sql
+++ Ngcs.Server/Ngcs.ElasticSearch.SampleDB/dbo/Tables/Courts.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Courts] (
     [id]    INT           NOT NULL,
     [name]  NVARCHAR (50) NOT NULL,
-    [level] NVARCHAR (50) NOT NULL,
+    [levelId] INT NOT NULL,
     CONSTRAINT [PK_Courts] PRIMARY KEY CLUSTERED ([id] ASC)
 );
